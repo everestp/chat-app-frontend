94184d0a8d67c875f61886d40e3c67f379cc39ab
Everest Paudel <paudeleverest09@gmail.com>
2025-04-22 23:08:49 +0545

    Feat: Update base URL for API requests to new production server

--- src/config/AxiosHelper.js
+++ src/config/AxiosHelper.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const baseURL = "https://chat-app-backend-rakt.onrender.com"
+export const baseURL = "https://chat-springboot-1.onrender.com"
 export const httpClient = axios.create({
   baseURL: baseURL,
 });
